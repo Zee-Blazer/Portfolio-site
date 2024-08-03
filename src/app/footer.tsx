a2793f6e8e10a8e300f3297de11befc0baf1bf58
Zee-Blazer <ganiyu.bo.bolaji@gmail.com>
2024-08-03 19:06:08 +0100

    Trying to introduce new fonts to the application

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -7,14 +7,14 @@ import { BsLinkedin } from "react-icons/bs";
 export default function Footer() {
     
     return (
-        <div className="mt-16">
+        <div className="mt-16 p-6">
             <div className="flex text-center justify-center">
                 <FaSquareFacebook className="text-2xl mx-2 cursor-pointer hover:text-blue-700" />
                 <FaInstagram className="text-2xl mx-2 cursor-pointer hover:text-pink-600" />
                 <FaXTwitter className="text-2xl mx-2 cursor-pointer hover:text-black" />
                 <BsLinkedin className="text-2xl mx-2 cursor-pointer hover:text-blue-500" />
             </div>
-            <p className="text-center mt-2">
+            <p className="text-center mt-3 text-sm">
                 Copyright @{new Date().getFullYear()} All rights reserved
             </p>
         </div>
