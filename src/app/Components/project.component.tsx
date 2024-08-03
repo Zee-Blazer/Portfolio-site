a529cf518b246f2e9c0fb2a25d79244f8a7fd6b4
Zee-Blazer <ganiyu.bo.bolaji@gmail.com>
2024-08-03 23:02:35 +0100

    On the project section

---
+++ src/app/Components/project.component.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+
+import img from "../Images/project.png";
+
+export default function Project() {
+
+    return (
+        <>
+            <h4>Featured works</h4>
+
+            <div>
+                <Image 
+                    className=""
+                    src={img} alt="No image" width={399} height={399} 
+                />
+                <div>
+                    <h1>Designing Dashboard</h1>
+                    <div>
+                        <p>2020</p>
+                        <p>Dasboard</p>
+                    </div>
+                    <p>
+                        Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. 
+                        Velit officia consequat duis enim velit mollit. 
+                        Exercitation veniam consequat sunt nostrud amet.
+                    </p>
+                </div>
+            </div>
+        </>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 
 import Intro from "./Components/intro.component";
 import Blog from "./Components/blog.component";
+import Project from "./Components/project.component";
 
 export default function Home() {
   return (
     <main className="">
       <Intro />
       <Blog />
+      <Project />
     </main>
   );
 }
