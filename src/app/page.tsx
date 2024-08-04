db29315cb62bcaf069ee7f71e8ad4e57ea796073
Zee-Blazer <ganiyu.bo.bolaji@gmail.com>
2024-08-04 22:14:13 +0100

    About to push to deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useRef, useEffect } from "react";
 
 import Intro from "./Components/intro.component";
 import Blog from "./Components/blog.component";
@@ -10,9 +11,34 @@ import { motion, useInView, useAnimation } from "framer-motion";
 
 export default function Home() {
 
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const mainControls = useAnimation();
+
+  useEffect( () => {
+    if(isInView){
+      mainControls.start("visible");
+    }
+  }, [isInView] )
+
   return (
-    <main className="">
-      <Intro />
+    <main className="" ref={ ref }>
+
+      <div ref={ ref }>
+        <motion.div 
+          variants={{
+            hidden: { opacity: 0, y: 72 },
+            visible: { opacity: 1, y: 0 },
+          }}
+          initial="hidden"
+          animate={ mainControls }
+          transition={{ duration: 0.5, delay: 0.25 }}
+        >
+          <Intro />
+        </motion.div>
+      </div>
+
       <motion.div
         variants={{
           hidden: { opacity: 0, y: 72 },
@@ -20,11 +46,23 @@ export default function Home() {
         }}
         initial="hidden"
         animate="visible"
-        transition={{ duration: 0.5, delay: 0.25 }}
+        transition={{ duration: 1.0, delay: 1.25 }}
       >
         <Blog />
       </motion.div>
-      <Project />
+
+      <motion.div
+        variants={{
+          hidden: { opacity: 0, y: 72 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        initial="hidden"
+        animate={ mainControls }
+        transition={{ duration: 1.8, delay: 1.85 }}
+      >
+        <Project />
+      </motion.div>
+    
     </main>
   );
 }
